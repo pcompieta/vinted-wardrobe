615c0740418e1112964c19adc81974e1c0494e6f
Paolo Compieta <paolo.compieta@accenture.com>
2025-10-05 23:44:05 +0200

    update data model to json format as of 2025, Oct 5th

--- src/pyVinted/items/item.py
+++ src/pyVinted/items/item.py
@@ -5,29 +5,43 @@ from pyVinted.requester import requester
 class Item:
     def __init__(self, data):
         self.raw_data = data
-        self.id = data["id"]
-        self.title = data["title"]
-        self.brand_title = data["brand_title"]
-        try:
-            self.size_title = data["size_title"]
-        except:
-            self.size_title = data["size_title"]
-        self.currency = data["price"]["currency_code"]
-        self.price = data["price"]["amount"]
-        self.photo = data["photo"]["url"]
-        self.url = data["url"]
-        self.created_at_ts = datetime.fromtimestamp(
-            data["photo"]["high_resolution"]["timestamp"], tz=timezone.utc
-        )
-        self.raw_timestamp = data["photo"]["high_resolution"]["timestamp"]
+        self.id = data.get("id")
+        self.title = data.get("title")
+        self.user_id = data.get("user_id")
+        self.is_draft = data.get("is_draft")
+        self.is_closed = data.get("is_closed")
+        self.is_reserved = data.get("is_reserved")
+        self.is_hidden = data.get("is_hidden")
+        self.price = data.get("price")
+        self.transaction_permitted = data.get("transaction_permitted")
+        self.is_processing = data.get("is_processing")
+        self.currency = data.get("currency")
+        self.item_closing_action = data.get("item_closing_action")
+        self.path = data.get("path")
+        self.user = data.get("user")
+        self.item_alert = data.get("item_alert")
+        self.item_alert_type = data.get("item_alert_type")
+        self.promoted = data.get("promoted")
+        self.is_business_user = data.get("is_business_user")
+        self.size = data.get("size")
+        self.view_count = data.get("view_count")
+        self.can_push_up = data.get("can_push_up")
+        self.push_up = data.get("push_up")
+        self.can_edit = data.get("can_edit")
+        self.stats_visible = data.get("stats_visible")
+        self.favourite_count = data.get("favourite_count")
+        self.is_favourite = data.get("is_favourite")
+        self.url = data.get("url")
+        self.service_fee = data.get("service_fee")
+        self.total_item_price = data.get("total_item_price")
+        self.status = data.get("status")
+        self.photos = data.get("photos", [])
+        self.brand = data.get("brand")
+        self.is_heavy_bulky = data.get("is_heavy_bulky")
+        self.item_box = data.get("item_box")
 
     def __eq__(self, other):
         return self.id == other.id
 
     def __hash__(self):
         return hash(('id', self.id))
-
-    def isNewItem(self, minutes=3):
-        delta = datetime.now(timezone.utc) - self.created_at_ts
-        return delta.total_seconds() < minutes * 60
-
